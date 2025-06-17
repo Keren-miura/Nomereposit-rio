05f0657d19779ee55aaa95e0c8a525b24e08e1d0
Keren-miura <Keren.miura@alunos.sc.senac.br>
2025-06-17 15:51:12 -0300

    Update ClasseTesteGit.java

--- ClasseTesteGit.java
+++ ClasseTesteGit.java
@@ -2,6 +2,8 @@ public class ClasseTesteGit{
 
        public static void main(String[] args){
                System.out.println("Outro commit");
+     System.out.println("Teste de pull");
+	
  
 	}
  
